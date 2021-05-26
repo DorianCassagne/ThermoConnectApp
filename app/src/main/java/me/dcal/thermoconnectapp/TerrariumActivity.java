bf856dd3f06ed628932a7c003511ace1ba1e5cd9
Fushina <thefushina@gmail.com>
2021-05-26 17:14:16 +0200

    Terrarium
    Dev Terra

--- app/src/main/java/me/dcal/thermoconnectapp/TerrariumActivity.java
+++ app/src/main/java/me/dcal/thermoconnectapp/TerrariumActivity.java
@@ -1,13 +1,20 @@
 package me.dcal.thermoconnectapp;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -29,6 +36,14 @@ import retrofit2.Response;
 public class TerrariumActivity extends AppCompatActivity {
 
     private BodyTerrarium bt;
+    private TimePickerDialog timeMinPickerDialog;
+    private TimePickerDialog timeMaxPickerDialog;
+    private TextView heureMaxTerrarium;
+    private TextView heureMinTerrarium;
+    private TextView TitleTerrarium;
+    private EditText TitleTerrariumEdit;
+    private LinearLayout globalLayout;
+    private Button save_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +55,21 @@ public class TerrariumActivity extends AppCompatActivity {
         BodyConnexion body=API.getBodyConnexion(getApplicationContext());
         Toast toast = Toast.makeText(getApplicationContext(), body.login , Toast.LENGTH_LONG);
         toast.show();
+        heureMaxTerrarium = (TextView)findViewById(R.id.HeureMaxTerrarium);
+        heureMinTerrarium = (TextView)findViewById(R.id.HeureMinTerrarium);
+        TitleTerrarium = (TextView)findViewById(R.id.TitleTerrarium);
+        TitleTerrariumEdit = (EditText)findViewById(R.id.TitleTerrariumEdit);
+        globalLayout = (LinearLayout)findViewById(R.id.screen);
+        save_button = (Button)findViewById(R.id.save_button);
+        TitleTerrariumEdit.setText(bt.nameTerrarium);
+        if(!TitleTerrariumEdit.hasFocus())
+            TitleTerrarium.setText(TitleTerrariumEdit.getText().toString());
+
+
         bt.setBodyConnexion(body);
         Call<List<BodyAnimal>> list = API.getInstance().simpleService.listAnimal(bt);
         ListView AnimalList = (ListView)findViewById(R.id.AnimalList);
         ArrayAdapter<BodyAnimal> arrayAdapter = new ArrayAdapter<BodyAnimal>(this, android.R.layout.simple_list_item_1);
-
         AnimalList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -69,6 +94,36 @@ public class TerrariumActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void clickTimeMin(View v){
+        API.launchShortToast(getApplicationContext(),"onClick");
+        timeMinPickerDialog=new TimePickerDialog(TerrariumActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minutes) {
+                if(minutes<=9){
+                    heureMinTerrarium.setText(hour+":0"+minutes);
+                }else{
+                    heureMinTerrarium.setText(hour+":"+minutes);
+                }
+            }
+        },Integer.parseInt(heureMinTerrarium.getText().toString().split(":")[0]),Integer.parseInt(heureMinTerrarium.getText().toString().split(":")[1]),true);
+        timeMinPickerDialog.show();
+    }
+    public void clickTimeMax(View v){
+        API.launchShortToast(getApplicationContext(),"onClick");
+        timeMaxPickerDialog=new TimePickerDialog(TerrariumActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minutes) {
+                if(minutes<=9){
+                    heureMaxTerrarium.setText(hour+":0"+minutes);
+                }else{
+                    heureMaxTerrarium.setText(hour+":"+minutes);
+                }
+
+            }
+        },Integer.parseInt(heureMaxTerrarium.getText().toString().split(":")[0]),Integer.parseInt(heureMaxTerrarium.getText().toString().split(":")[1]),true);
+        timeMaxPickerDialog.show();
     }
 
     @Override
@@ -140,6 +195,13 @@ public class TerrariumActivity extends AppCompatActivity {
                 API.launchShortToast(getApplicationContext(), "KO");
             }
         });
+    }
+
+    private void ChangerTitre(){
+        TitleTerrarium.setWidth(0);
+        TitleTerrarium.setHeight(0);
+        TitleTerrariumEdit.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+        TitleTerrariumEdit.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
     }
 
 }
